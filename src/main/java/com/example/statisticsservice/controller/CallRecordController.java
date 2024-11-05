6d5b9707371f13374cf77fd83c559fbdac1776b8
WonJun <duftlagltkfwk7@gmail.com>
2024-11-05 17:57:30 +0900

    Feature: CallRecord 컨트롤러 기본 내용 구성 및 테스트용 핸들러 추가

--- src/main/java/com/example/statisticsservice/controller/CallRecordController.java
+++ src/main/java/com/example/statisticsservice/controller/CallRecordController.java
@@ -1,7 +1,73 @@
 package com.example.statisticsservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.statisticsservice.domain.CallRecord;
+import com.example.statisticsservice.response.CallTimePerDateRes;
+import com.example.statisticsservice.service.CallRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/records")
 public class CallRecordController {
+
+    private final CallRecordService callRecordService;
+
+    @Autowired
+    public CallRecordController(CallRecordService callRecordService) {
+        this.callRecordService = callRecordService;
+    }
+
+
+    public static class RecordReq {
+        private LocalDateTime createdTime;
+        private Long callDuration; // 초 단위
+
+    }
+
+/*  ToDo
+    @GetMapping("/{phoneNumber}")
+    public CallTimePerDateRes getCallTimePerDate(@PathVariable String phoneNumber) {
+        callRecordService.getAllrecords();
+
+        CallTimePerDateRes res = CallTimePerDateRes();
+        return res;
+    }
+
+    @GetMapping("records/{index}")
+    public String read(@PathVariable String index) {
+
+    }
+*/
+
+//    @PutMapping("records/{index}")
+//    public String update(@PathVariable String index) {
+//
+//    }
+//
+//    @DeleteMapping("records/{index}")
+//    public String delete(@PathVariable String index) {
+//
+//    }
+
+
+
+    @GetMapping("/t")
+    public String wj_test() {
+        // data 추가
+        
+        // 추가한 데이터 로드
+        Optional<CallRecord> c = callRecordService.getRecordById(333L);
+        System.out.println("로드한 데이터: "+c.toString());
+        System.out.println(Arrays.toString(callRecordService.getTopRecordsByCallDuration().getFirst()));
+        //추가한 데이터 제거
+
+
+        return "test complete!";
+    }
+
 }
