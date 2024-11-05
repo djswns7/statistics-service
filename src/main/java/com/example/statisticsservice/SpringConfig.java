2b74e7426b8c9f7d87c0ba91a72f9835e922ec9b
WonJun <duftlagltkfwk7@gmail.com>
2024-11-05 17:56:37 +0900

    Feature: 요청응답 테스트를 위한 웹 시큐리티 임시 해제

--- src/main/java/com/example/statisticsservice/SpringConfig.java
+++ src/main/java/com/example/statisticsservice/SpringConfig.java
@@ -1,9 +1,28 @@
 package com.example.statisticsservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/public/**").permitAll() // 특정 경로에 대한 인증 비활성화
+                                .requestMatchers("/record/**").permitAll() // 레코드 경로 테스트중
+                                .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                )
+                .formLogin(formlogin -> formlogin.disable()) // 기본 로그인 폼 활성화
+                .csrf(csrf -> csrf.disable()); // CSRF 비활성화 (필요 시)
+
+        return http.build();
+    }
 }
