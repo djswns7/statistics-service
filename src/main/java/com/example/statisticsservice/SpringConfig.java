c8df895e2a36d682f2e4cf67727c45651adfa3a9
WonJun <duftlagltkfwk7@gmail.com>
2024-11-19 17:26:38 +0900

    Feature: GoogleAuthenticator 클래스 주입을 위한 Bean 등록

--- src/main/java/com/example/statisticsservice/SpringConfig.java
+++ src/main/java/com/example/statisticsservice/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.example.statisticsservice;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -16,7 +17,7 @@ public class SpringConfig {
         http
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/public/**").permitAll() // 특정 경로에 대한 인증 비활성화
+                                .requestMatchers("/**").permitAll() // 특정 경로에 대한 인증 비활성화
                                 .requestMatchers("/call-records/**").permitAll() // 레코드 경로 테스트중
                                 .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
@@ -24,5 +25,10 @@ public class SpringConfig {
                 .csrf(csrf -> csrf.disable()); // CSRF 비활성화 (필요 시)
 
         return http.build();
+    }
+
+    @Bean
+    public GoogleAuthenticator googleAuthenticator() {
+        return new GoogleAuthenticator();
     }
 }
